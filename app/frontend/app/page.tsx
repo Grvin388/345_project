a93e6dc02d1621abe0a2dfc11eff6ea8919da689
Gavin Chock-Chiong <gavin.chockchiong@gmail.com>
2026-04-03 14:49:21 -0400

    removing an import in page.tsx for testing

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import { EventsPage } from "@/features/events/pages/EventPages";
-import { LoginPage } from "@/features/auth/pages/LoginPage";
+//import { LoginPage } from "@/features/auth/pages/LoginPage";
 
 export default function Home() {
   return <EventsPage />;
